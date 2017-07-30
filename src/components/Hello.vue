2d133f0b60db843bf61063fb733d665e2317cc2f
Doug Black <dblack@americanbible.org>
2017-07-30 06:24:40 -0400

    Updated Hello vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,45 +1,35 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="frontpage">
+    <h1>Hi, I'm Doug</h1>
+    <h2>Full Stack Web Developer</h2>
+    <p>I make machines serve their human masters, typically with web and mobile applications! Skilled in: PHP, ExpressionEngine, Laravel, Wordpress, Vue.js, Javascript, Ionic/Angular, and various others. If you have a problem, I can solve it.</p>
+    <div v-for='site in sites'>
+      <h3>{{site.name}}</h3>
+      <h3>{{site.url}}</h3>
+    </div>
+    <h2><a href="http://dougblackjr.com/#contact">Contact me to get started!</a></h2>
   </div>
 </template>
 
 <!-- JS -->
 <script>
+import card from 'Card.vue'
+import siteslist from '../data/sites.js'
+
 export default {
   name: 'hello',
+  components: {
+    'card': card
+  },
   data () {
     return {
-      sites: {
-        'aceshomeimprovementllc.com': 'aces.png'
-      }
+      sites: siteslist
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="sass">
-body {
-  background-image: url(../assets/port-bg.png);
-  background-size: cover;
-  background-repeat: no-repeat;
-}
-h1, h2 {
-  font-weight: normal;
-}
+<style scoped lang="scss">
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
