88eec0601732fab05e4a2a5fdb95d163253daeb5
Doug Black <dblack@americanbible.org>
2017-07-30 06:24:32 -0400

    Moved sites to data folder

---
+++ src/data/sites.js
@@ -0,0 +1,42 @@
+[
+  {
+    name: 'Aces Home Improvement',
+    url: 'aceshomeimprovementllc.com',
+    image: 'aces.png',
+    thumb: '',
+    mobileImage: 'aces_mobile.png',
+    description: 'Home improvement site, simple contact form'
+  },
+  {
+    name: 'Doug Black',
+    url: 'dougblackjr.com',
+    image: 'dougblackjr.png',
+    thumb: '',
+    mobileImage: 'dougblackjr_mobile.png',
+    description: ''
+  },
+  {
+    name: 'Never Give Up',
+    url: 'nevergiveuppa.com',
+    image: 'ngu.png',
+    thumb: '',
+    mobileImage: 'ngu_mobile.png',
+    description: ''
+  },
+  {
+    name: 'Joe Ritchie General Contractor',
+    url: 'joeritchiegeneralcontractor.net',
+    image: 'joe.png',
+    thumb: '',
+    mobileImage: '',
+    description: ''
+  },
+  {
+    name: '',
+    url: '',
+    image: '',
+    thumb: '',
+    mobileImage: '',
+    description: ''
+  }
+]
