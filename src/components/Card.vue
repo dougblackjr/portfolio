5300004e95997476924317fb39b5e635e5d50f02
Doug Black <dblack@americanbible.org>
2017-07-30 06:24:46 -0400

    Initial Card styling

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="card" :style="{ backgroundImage: `url(${imgSrc})` }">
+		<a class="card--link" v-link="">
+			<h3 class="card--title">{{ project.name}}</h3>
+		</a>
+	</div>
+</template>
+
+<script>
+
+export default {
+
+	props: ['project'],
+
+	data() {
+		return {
+			imgSrc: require('../sites/${this.project.thumb}')
+		}
+	}
+
+}
+
+</script>
