180cb3067557f11219d09c08de30aefb21599cc4
Doug Black <dblack@americanbible.org>
2017-07-30 18:41:31 -0400

    Moved to separate json

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -24,42 +24,7 @@
 <!-- JS -->
 <script>
 import card from './Card.vue'
-
-// Set vars
-var sites = [
-  {
-    name: 'Aces Home Improvement',
-    url: 'aceshomeimprovementllc.com',
-    image: 'aces.png',
-    thumb: '',
-    mobileImage: 'aces_mobile.png',
-    description: 'Home improvement site, simple contact form'
-  },
-  {
-    name: 'Doug Black',
-    url: 'dougblackjr.com',
-    image: 'dougblackjr.png',
-    thumb: '',
-    mobileImage: 'dougblackjr_mobile.png',
-    description: ''
-  },
-  {
-    name: 'Never Give Up',
-    url: 'nevergiveuppa.com',
-    image: 'ngu.png',
-    thumb: '',
-    mobileImage: 'ngu_mobile.png',
-    description: ''
-  },
-  {
-    name: 'Joe Ritchie General Contractor',
-    url: 'joeritchiegeneralcontractor.net',
-    image: 'joe.png',
-    thumb: '',
-    mobileImage: '',
-    description: ''
-  }
-]
+import sites from '../data/sites.json'
 
 export default {
   name: 'hello',
