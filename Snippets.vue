84f4c5896ed758ef872034d42ee3c1a64a2b6472
Doug Black <dougblackjr@gmail.com>
2017-07-31 17:44:08 -0400

    Updated for storytelling

--- Snippets.vue
+++ Snippets.vue
@@ -1,1 +1,14 @@
 { name: 'project_component', params: { projectId: project_id } }
+
+
+DATA TEMPLATE
+  "namealllowercasenospaces": {
+    "name": "",
+    "url": "",
+    "image": "",
+    "mobileimage": "",
+    "keywords": "",
+    "problem": "",
+    "solution": "",
+    "description": ""
+  },
